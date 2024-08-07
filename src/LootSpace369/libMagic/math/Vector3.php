29c16edc6520800645b8553b2aa9d8286cf96501
LSpace <tbao12901@gmail.com>
2024-08-07 14:46:07 +0700

    Create Vector3.php

---
+++ src/LootSpace369/libMagic/math/Vector3.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LootSpace369\libMagic\math;
+
+use pocketmine\math\Vector3 as Vector;
+
+class Vector3 as Vector {
+
+    /**
+  	 * @return a Vector3 instance
+  	 */
+    public function asVector3() : Vector3 {
+      return new Vector3($this->x, $this->y, $this->z);
+    }
+}
