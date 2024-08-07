2f0489aea804ebe469889a66513b6dbfd9ed178d
LSpace <tbao12901@gmail.com>
2024-08-07 10:14:19 +0700

    Update RegisterResourcePack.php

--- src/LootSpace369/libMagic/pack/RegisterResourcePack.php
+++ src/LootSpace369/libMagic/pack/RegisterResourcePack.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace LootSpace369\libMagic;
 
+use pocketmine\plugin\PluginBase;
 use pocketmine\resourcepacks\ZippedResourcePack;
 use Symfony\Component\Filesystem\Path;
 
 final class RegisterResourcePack {
   
-    public static function register(string $path, string $packName) {
-        $rpManager = $this->getServer()->getResourcePackManager();
-		$rpManager->setResourceStack($rpManager->getResourceStack() + [new ZippedResourcePack(Path::join($path, $packName))]);
-		($serverForceResources = new \ReflectionProperty($rpManager, "serverForceResources"))->setAccessible(true);
-		$serverForceResources->setValue($rpManager, true);
+    public static function register(PluginBase $plugin, string $path, string $packName) {
+        $rpManager = $plugin->getServer()->getResourcePackManager();
+	$rpManager->setResourceStack($rpManager->getResourceStack() + [new ZippedResourcePack(Path::join($this->getDataFolder(), "Boss.mcpack"))]);
+	($serverForceResources = new \ReflectionProperty($rpManager, "serverForceResources"))->setAccessible(true);
+	$serverForceResources->setValue($rpManager, true);
     }
 }
