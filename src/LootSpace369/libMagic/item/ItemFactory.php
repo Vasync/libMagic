c638724049fb3c9dfce6d446f81669537516d7fa
Vasync <tbao12901@gmail.com>
2024-08-31 16:03:21 +0700

    Update ItemFactory.php

--- src/LootSpace369/libMagic/item/ItemFactory.php
+++ src/LootSpace369/libMagic/item/ItemFactory.php
@@ -7,9 +7,11 @@ namespace LootSpace369\libMagic\item;
 use pocketmine\item\LegacyStringToItemParser;
 use pocketmine\item\Item;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\utils\SingletonTrait;
 
 class ItemFactory {
-
+    use SingletonTrait;
+    
     public function get(int $id, int $meta = 0, int $count = 1, ?CompoundTag $tags = null) : Item {
         #PM3 converted
 
